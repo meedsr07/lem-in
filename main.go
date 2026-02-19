14eca450e4000cc5862b525ff66605d018704482
msarar <mohammedsarar7@gmail.com>
2026-02-19 11:32:03 +0000

    adding anted numbre

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -18,7 +18,43 @@ func main() {
 		fmt.Println("error in reading argument")
 		return
 	}
+	if len(file) == 0 {
+		fmt.Println("error empty file")
+		return
+	}
 	conten := strings.TrimSpace(string(file))
-	line := strings.Split(conten, "\n")
-	fmt.Println(line)
+	lines := strings.Split(conten, "\n")
+
+	antNbr := GetAnts(lines)
+	if antNbr == 0 {
+		return
+	}
+	fmt.Printf("Number of ants: %d\n", antNbr)
+
+}
+
+func GetAnts(lines []string) int {
+    for _, line := range lines {
+        line = strings.TrimSpace(line)
+        if line == "" || strings.HasPrefix(line, "#") {
+            continue 
+        }
+
+        fields := strings.Fields(line)
+        if len(fields) != 1 {
+            fmt.Println("ERROR: invalid data format")
+            return 0
+        }
+
+        antNbr, err := strconv.Atoi(fields[0])
+        if err != nil || antNbr <= 0 {
+            fmt.Println("ERROR: invalid data format")
+            return 0
+        }
+
+        return antNbr 
+    }
+
+    fmt.Println("ERROR: invalid data format") 
+    return 0
 }
