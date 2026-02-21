99f8f6a4f6e0ab77683859f72b6a748bacafe6e9
msarar <mohammedsarar7@gmail.com>
2026-02-21 19:06:51 +0000

    add a new changes in project

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -11,37 +10,6 @@ func main() {
 		fmt.Println("Error invalid numbre of argument")
 		return
 	}
-	argument  := os.Args[1]
-	file , err := os.ReadFile(argument)
-	if err != nil  {
-		fmt.Println("error in reading argument")
-		return
-	}
-	if len(file) == 0 {
-		fmt.Println("error empty file")
-		return
-	}
-	conten := strings.TrimSpace(string(file))
-	lines := strings.Split(conten, "\n")
-
-	antNbr, lineIndex := GetAnts(lines)
-	if lineIndex == -1 {
-		return
-	}
-	if lineIndex == len(lines)-1 {
-		fmt.Println("ERROR: invalid data format")
-		return
-	}
-	if antNbr == 0 {
-		fmt.Println("ERROR: invalid data format")
-		return
-	}
-	if !CheckStartandEnd(lines) {
-		return
-	}
-	room := GetRoom(lines , lineIndex)
-	
-	fmt.Println("Number of ants:",antNbr , "line index:", lineIndex , room)
-
+	argument := os.Args[1]
+	Validation(argument)
 }
-
---
+++ validation.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Validation(arg string) {
+	file, err := os.ReadFile(arg)
+	if err != nil {
+		fmt.Println("error in reading file")
+		return
+	}
+	if len(file) == 0 {
+		fmt.Println("error : file is empty")
+		return
+	}
+	conten := strings.TrimSpace(string(file))
+	lines := strings.Split(conten, "\n")
+
+	antNbr, lineIndex := GetAnts(lines)
+	if lineIndex == -1 {
+		return
+	}
+	if lineIndex == len(lines)-1 {
+		fmt.Println("ERROR: invalid data format")
+		return
+	}
+	if antNbr == 0 {
+		fmt.Println("ERROR: invalid data format")
+		return
+	}
+	if !CheckStartandEnd(lines) {
+		return
+	}
+	room := GetRoom(lines, lineIndex)
+
+	fmt.Println("Number of ants:", antNbr, "line index:", lineIndex, room)
+}
