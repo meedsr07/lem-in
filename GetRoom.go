8ce801ee8a0aee4fd6d6a3dbf229852db2a3790d
msarar <mohammedsarar7@gmail.com>
2026-02-22 13:13:00 +0000

    add start and end

--- GetRoom.go
+++ GetRoom.go
@@ -6,34 +6,51 @@ import (
 )
 
 type Room struct {
-	Name string
-	X    int
-	Y    int
+	Name    string
+	X       int
+	Y       int
+	IsStart bool
+	IsEnd   bool
 }
 
 func GetRoom(lines []string, startindex int) []Room {
 	var res []Room
+	isstart := false
+	isend := false
 	for i := startindex; i < len(lines); i++ {
-		line  := strings.TrimSpace(lines[i])
-		if line == "" || strings.HasPrefix(line,"#") {
+		line := strings.TrimSpace(lines[i])
+		if line == ""  {
 			continue
 		}
-		if strings.Contains(line , "-") {
+		if line == "##start" {
+			isstart = true
+		}
+		if line == "##end" {
+			isend = true
+		}
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		if strings.Contains(line, "-") {
 			break
 		}
 		part := strings.Fields(line)
 		if len(part) != 3 {
 			continue
 		}
-		x , _ := strconv.Atoi(part[1])
-		y , _ := strconv.Atoi(part[2])
+		x, _ := strconv.Atoi(part[1])
+		y, _ := strconv.Atoi(part[2])
 
-		roomData := Room {
+		roomData := Room{
 			Name: part[0],
-			X: x,
-			Y: y,
+			X:    x,
+			Y:    y,
+			IsStart: isstart,
+			IsEnd: isend,
 		}
 		res = append(res, roomData)
+		isstart = false
+		isend = false
 
 	}
 	return res
