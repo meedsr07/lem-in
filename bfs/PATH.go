3686ccbc6a980a7f7a2696cf6f7130c34fbba3e8
msarar <mohammedsarar7@gmail.com>
2026-03-04 16:07:52 +0000

    a

--- bfs/PATH.go
+++ bfs/PATH.go
@@ -1,62 +1,35 @@
 package bfs
 
 
-// BFS finds one shortest path
-func bfsPath(graph map[string][]string, start, end string) []string {
+func BFSAllPaths(graph map[string][]string, start, end string) [][]string {
 	queue := [][]string{{start}}
-	visited := make(map[string]bool)
-	visited[start] = true
+	var allPaths [][]string
 
 	for len(queue) > 0 {
 		path := queue[0]
 		queue = queue[1:]
-		current := path[len(path)-1]
+		last := path[len(path)-1]
 
-		if current == end {
-			return path
+		if last == end {
+			allPaths = append(allPaths, path)
 		}
 
-		for _, neighbor := range graph[current] {
-			if !visited[neighbor] {
-				visited[neighbor] = true
+		for _, neighbor := range graph[last] {
+			if !contains(path, neighbor) {
 				newPath := append([]string{}, path...)
 				newPath = append(newPath, neighbor)
 				queue = append(queue, newPath)
 			}
 		}
 	}
-
-	return nil
+	return allPaths
 }
 
-// findMultiplePaths finds multiple non-overlapping shortest paths
-func FindMultiplePaths(graph map[string][]string, start, end string) [][]string {
-	var allPaths [][]string
-	// create a copy of the graph to modify
-	graphCopy := make(map[string][]string)
-	for k, v := range graph {
-		graphCopy[k] = append([]string{}, v...)
-	}
-
-	for {
-		path := bfsPath(graphCopy, start, end)
-		if path == nil {
-			break
-		}
-		allPaths = append(allPaths, path)
-
-		// remove edges of this path from the graph to avoid overlap
-		for i := 0; i < len(path)-1; i++ {
-			from, to := path[i], path[i+1]
-			newNeighbors := []string{}
-			for _, n := range graphCopy[from] {
-				if n != to {
-					newNeighbors = append(newNeighbors, n)
-				}
-			}
-			graphCopy[from] = newNeighbors
+func contains(path []string, node string) bool {
+	for _, n := range path {
+		if n == node {
+			return true
 		}
 	}
-
-	return allPaths
+	return false
 }
